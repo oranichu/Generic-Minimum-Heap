054a6c1ba1b9c36a7e77fcb3e31eb4c28b6c9f0c
Oran Nahoum <33873115+oranichu@users.noreply.github.com>
2018-06-09 19:11:24 +0300

    Index change WORKS !!!

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ class SaveIndex {
     int data ;
     int index ;
 public:
-    SaveIndex() : data(0), index(0) {} ;
+    SaveIndex(int d) : data(d), index(0) {} ;
     void setData(int i) {data = i ;};
     int getData() {return data ;} ;
 
@@ -17,26 +17,26 @@ public:
 
 };
 
-std::ostream& operator<<(std::ostream &os,SaveIndex s) {
-    return os<<s.getData() ;
+std::ostream& operator<<(std::ostream &os,SaveIndex* s) {
+    return os<<s->getData() ;
 }
 
 class DoNothing {
 public:
-    void operator()(SaveIndex a,int j){
-        a.setIndex(j) ;
+    void operator()(SaveIndex* a,int j){
+        a->setIndex(j) ;
     }
 };
 
-void printArr(SaveIndex arr[])  {
+void printArr(SaveIndex *arr[])  {
     for (int i = 0; i < 10; i++) {
-        std::cout << arr[i].getData() << " ";
+        std::cout << arr[i]->getData() << " ";
     }
     std::cout << std::endl;
     std::cout << "**********************"<< std::endl;
 
     for (int i = 0; i < 10; i++) {
-        std::cout << arr[i].getIndex() << " ";
+        std::cout << arr[i]->getIndex() << " ";
     }
     std::cout << std::endl;
 }
@@ -46,13 +46,17 @@ void printArr(SaveIndex arr[])  {
 int main() {
 
     int key[10] = {6,8,12,17,0,3,10,4,5,2};
-    SaveIndex data1[10] ;
+    SaveIndex *data1[10] ;
     for (int i=0 ; i< 10 ; i++) {
-        data1[i].setData(key[i]);
+        data1[i]=new SaveIndex(key[i]);
     }
     printArr(data1);
-    MinHeap<SaveIndex,int,DoNothing> h (data1,key,10,DoNothing()) ;
+    MinHeap<SaveIndex*,int,DoNothing> h (data1,key,10,DoNothing()) ;
     h.printHeap();
+
+    std::cout << "**********************" << std::endl ;
+
+    printArr(data1);
 
     return 0;
 }
