c69c5822d19a0e708d2fb028d2d6db3f019f43b9
Oran Nahoum <33873115+oranichu@users.noreply.github.com>
2018-06-09 20:00:55 +0300

    Insert Works.

--- main.cpp
+++ main.cpp
@@ -50,13 +50,26 @@ int main() {
     for (int i=0 ; i< 10 ; i++) {
         data1[i]=new SaveIndex(key[i]);
     }
-    printArr(data1);
-    MinHeap<SaveIndex*,int,DoNothing> h (data1,key,10,DoNothing()) ;
-    h.printHeap();
 
-    std::cout << "**********************" << std::endl ;
+    SaveIndex *data2[10] ;
+    for (int i=0 ; i< 10 ; i++) {
+        data2[i]=new SaveIndex(key[i]);
+    }
+
+    MinHeap<SaveIndex*,int,DoNothing> h (data1,key,4,DoNothing()) ;
+
+
+    h.insert(data1[4],key[4]);
+
+    for (int i=5 ; i< 10 ; i++) {
+        h.insert(data1[i],key[i]);
+    }
 
     printArr(data1);
+
+    MinHeap<SaveIndex*,int,DoNothing> h2 (data2,key,10,DoNothing()) ;
+    printArr(data2);
+
 
     return 0;
 }
